a5b4a92584833507e503ef3ad038a862a57e3a7d
sideways.mov <l.lbruzga.b@gmail.com>
2024-02-07 17:44:20 +0200

    initial commit

---
+++ ItemRental.Core/Entities/Item.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ItemRental.Core.Entities
+{
+    public class Item : Entity
+    {
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public string[] Tags { get; set; } = new string[0];
+        public string[] Photos { get; set; } = new string[0];
+    }
+}
---
+++ ItemRental.Core/Entities/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ItemRental.Core.Entities
+{
+    public class User : Entity
+    {
+        public string UserName { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string AvatarURL { get; set; } = string.Empty;
+    }
+}
