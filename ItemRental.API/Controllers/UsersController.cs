fc1bed3d2306692dbb368e41e5db9abf11cecf15
sideways.mov <l.lbruzga.b@gmail.com>
2024-02-25 17:53:41 +0200

    Add user controller

---
+++ ItemRental.API/Controllers/UsersController.cs
@@ -0,0 +1,42 @@
+﻿using ItemRental.Application.Users;
+using ItemRental.Core.Helpers;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ItemRental.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController: ControllerBase
+    {
+        private readonly ISender _sender;
+        public UsersController(ISender sender)
+        {
+            _sender = sender;
+        }
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody] LoginRequest request)
+        {
+            Result<string> result = await _sender.Send(new LoginCommand(request.Email));
+
+            if(result.IsFailure)
+            {
+                return BadRequest(result.Error);
+            }
+
+            return Ok(result.Value);
+        }
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(Guid id)
+        {
+            Result<UserResponse> result = await _sender.Send(new GetUserByIdQuery(id));
+
+            if(result.IsFailure)
+            {
+                return NotFound(result.Error);
+            }
+
+            return Ok(result.Value);
+        }
+    }
+}
