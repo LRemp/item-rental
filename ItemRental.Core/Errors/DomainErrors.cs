2f163a0984e9da086dd4af0cf38e656289e38fb9
sideways.mov <l.lbruzga.b@gmail.com>
2024-02-25 17:53:09 +0200

    Add domain errors

---
+++ ItemRental.Core/Errors/DomainErrors.cs
@@ -0,0 +1,22 @@
+﻿using ItemRental.Core.Helpers;
+
+namespace ItemRental.Core.Errors
+{
+    public static class DomainErrors
+    {
+        public static class User
+        {
+            public static readonly Error EmailAlreadyInUse = new Error(
+                "User.EmailAlreadyInUse",
+                "The provided email is already in use.");
+
+            public static readonly Func<Guid, Error> NotFound = id => new Error(
+                "User.NotFound",
+                $"The member with identifier {id} was not found.");
+            
+            public static readonly Error InvalidCredentials = new Error(
+                "User.InvalidCredentials", 
+                "The provided credentials are invalid");
+        }
+    }
+}
